db97ea479b0cedf144de0fce7bda0bcfc8118256
msieur-gab <baude.gabriel@gmail.com>
2025-08-10 12:12:02 +0200

    Set debug to false

--- config.js
+++ config.js
@@ -18,7 +18,7 @@
  */
 export const config = {
     // General Settings
-    DEBUG_MODE: flase,
+    DEBUG_MODE: false,
 
     // NFC URL Settings
     FORCE_NFC_URL_BASE: 'https://msieur-gab.github.io/peeble/kid.html',
